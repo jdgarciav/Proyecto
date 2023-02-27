aacdc29b0d224dfa51fd14d68a1ee18326bf2228
jdgarciav <jdgarciav@minsait.com>
2023-02-27 01:39:44 -0600

    Tienda repository added

---
+++ msvc-tienda/src/main/java/com/minsait/msvctienda/repositories/TiendaRepository.java
@@ -0,0 +1,8 @@
+package com.minsait.msvctienda.repositories;
+
+import com.minsait.msvctienda.models.entity.Tienda;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TiendaRepository extends JpaRepository<Tienda,Long> {
+
+}
