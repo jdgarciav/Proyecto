a3435a137bbe3f5e7a17ddce07c6085fa4d40c67
jdgarciav <jdgarciav@minsait.com>
2023-02-27 00:44:24 -0600

    msvc-tienda completed

---
+++ msvc-tienda/src/main/java/com/minsait/msvctienda/MsvcTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.minsait.msvctienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcTiendaApplication.class, args);
+	}
+
+}
---
+++ msvc-tienda/src/test/java/com/minsait/msvctienda/MsvcTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minsait.msvctienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
